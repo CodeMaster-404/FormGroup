5d4d369f89c800249ddc6acb918e9460f3d58ece
CodeMaster-404 <74175097+CodeMaster-404@users.noreply.github.com>
2021-01-20 13:53:16 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { HomescreenComponent } from './main/homescreen/homescreen.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomescreenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  exports: [
+
   ],
   providers: [],
   bootstrap: [AppComponent]
